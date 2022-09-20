ff3074ae12a50bd0c8222370193e1d3e285de206
Cleverson Nahum <cleversonahum@gmail.com>
2022-09-20 15:16:47 -0300

    downloading datasets and git repo directly inside the VM

--- init.sh
+++ init.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
+# Updating and installing Graphviz
 sudo apt update -y
 sudo apt install -y graphviz
+
+# Cloning github repository
+git clone https://github.com/aldebaro/ai6g.git
+
+# Download dataset
+wget https://nextcloud.lasseufpa.org/s/4JEB9M5amDZg3XX/download/ai6g.zip
+
+# Unzip files
+unzip -o ai6g.zip
+
+# Installing Conda and generating conda env
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -u -p /home/vagrant/miniconda3/
 echo 'export PATH=/home/vagrant/miniconda3/bin:$PATH' >> ~/.profile 
