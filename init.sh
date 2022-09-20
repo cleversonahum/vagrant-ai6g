9e90b2bb415c960da17542783dd8bab94d7db269
Cleverson Nahum <cleversonahum@gmail.com>
2022-09-20 13:13:15 -0300

    installing graphviz

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+sudo apt update -y
+sudo apt install -y graphviz
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -b -u -p /home/vagrant/miniconda3/
 echo 'export PATH=/home/vagrant/miniconda3/bin:$PATH' >> ~/.profile 
